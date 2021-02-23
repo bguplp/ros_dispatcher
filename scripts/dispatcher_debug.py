125fd89975639eae01bf898f3fb845bc4d99d4e7
Tal Feiner <talfe@post.bgu.ac.il>
2021-02-23 16:17:21 +0200

    update

--- scripts/dispatcher_debug.py
+++ scripts/dispatcher_debug.py
@@ -12,7 +12,6 @@ from robotican_demos_upgrade.srv import sense_object, sense_objectResponse
 from robotican_demos_upgrade.srv import move_to_point, move_to_pointResponse
 clear_octomap = rospy.ServiceProxy('/clear_octomap', Empty)
 
-
 def pick_unknown_action(robot, can, location):
     print "pick execute start"
     rospy.wait_for_service('pick_unknown')
@@ -22,8 +21,7 @@ def pick_unknown_action(robot, can, location):
         print "pick result: "+resp1.result
         return resp1.result
     except rospy.ServiceException, e:
-        print "Service call failed: %s" % e
-
+        print "Service call failed: %s"%e
 
 def place_unknown_action(robot, can, obj_on, location):
     print "place execute start"
@@ -34,8 +32,7 @@ def place_unknown_action(robot, can, obj_on, location):
         print "pick result: "+resp1.result
         return resp1.result
     except rospy.ServiceException, e:
-        print "Service call failed: %s" % e
-
+        print "Service call failed: %s"%e
 
 def robot_navigation_action(robot, location, destination, floor="1"):
     print "move_to_point execut start"
@@ -46,28 +43,27 @@ def robot_navigation_action(robot, location, destination, floor="1"):
         print "move_to_point result: " + resp1.result
         return resp1.result
     except rospy.ServiceException, e:
-        print "Service call failed: %s" % e
-
+        print "Service call failed: %s"%e
 
 def sense_object_action(robot, can, location):
     print "observe_can execut start"
     rospy.wait_for_service('sense_object')
     try:
         observe_can_proxy = rospy.ServiceProxy('sense_object', sense_object)
-        resp1 = observe_can_proxy(robot, location, can)
+        resp1 = observe_can_proxy(robot, can, location)
         print "observe_can result: "+resp1.result
         return resp1.result
     except rospy.ServiceException, e:
-        print "Service call failed: %s" % e
+        print "Service call failed: %s"%e
 
 
 def dispatcher(service_name, *args):
-    # print "module name:"
-    # print sys.modules[__name__]
-    # print "module att:"
-    # print(dir(sys.modules[__name__]))
-    # import pdb;
-    # pdb.set_trace()
+    #print "module name:"
+    #print sys.modules[__name__]
+    #print "module att:"
+    #print(dir(sys.modules[__name__]))
+    #import pdb;
+    #pdb.set_trace()
     method_to_call = getattr(sys.modules[__name__], service_name + '_action')
     observation = method_to_call(*args)
     return service_name + ':' + observation
@@ -78,36 +74,36 @@ def handle_client_connection(client_socket):
         print ' '
         print ' '
         print '-----------------------------------------------------------------------------------------------'
-        request = client_socket.recv(1024)
-        print 'Received {}'.format(request)
-        args = request.split(',')
+        # request = client_socket.recv(1024)
+        # print 'Received {}'.format(request)
+        # args = request.split(',')
+        args = client_socket
         action_name = args[0]
         args.pop(0)
-        print "start calling:" + action_name
+        print "start calling:" +action_name
         observation = dispatcher(action_name, *args)
         print "finished:" + action_name
         print "action observation:" + observation
         print "response to planner:'" + observation
-        client_socket.send(observation)
-        client_socket.close()
-        clear_octomap()     # flush octomap data
-        p = Popen(['rosnode', 'cleanup'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)   # clean ros node and topics
-        while p.poll() is None:     # clean ros node and topics
-            p.communicate(input=b'y\n')     # clean ros node and topics
-        p.stdout.close()        # clean ros node and topics
-        p.stdin.close()         # clean ros node and topics
+        # client_socket.send(observation)
+        # client_socket.close()
+        # clear_octomap()     #   flush octomap data
+        p = Popen(['rosnode', 'cleanup'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)   #   clean ros node and topics
+        while p.poll() is None:     #   clean ros node and topics
+            p.communicate(input=b'y\n')     #   clean ros node and topics
+        p.stdout.close()        #   clean ros node and topics
+        p.stdin.close()         #   clean ros node and topics
         print 'connection ended'
     except Exception as e:
         print 'exception thrown: ', e
-        client_socket.close()
+        # client_socket.close()
         print 'connection ended'
 
-
 def start_tcp_server(host, port):
-    # import pdb;
-    # pdb.set_trace()
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #import pdb;
+    #pdb.set_trace()
+    #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    server= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
     print('bind executed')
     server.listen(100)
@@ -130,19 +126,26 @@ def start_tcp_server(host, port):
     print('TCP server closed')
 
 
+
 if __name__ == "__main__":
-    # dispatcher("pick", *['robot2','obj2','location2'])
-    # dispatcher("move_to_point", *['robot4','loc4','dest4','floor4'])
+    #dispatcher("pick", *['robot2','obj2','location2'])
+    #dispatcher("move_to_point", *['robot4','loc4','dest4','floor4'])
     # pick_client("1", "2", "3")
 
-    try:
-        HOST = socket.gethostbyname("localhost")
-        PORT = 1770
-        print('HOST:', HOST)
-        start_tcp_server(HOST, PORT)
-    except rospy.ROSInterruptException:
-        pass
-    # pick_client("$robot1", "$object1","$location1")
+    # a = ["place_unknown", "robot", "can", "table", "outside_lab211"]
+    # a = ["sense_object", "robot", "can", "corridor"]
+    # a = ["pick_unknown", "robot", "can", "corridor"]
+    a = ["robot_navigation", "robot", "corridor", "auditorium", "1"]
+    # a = ["robot_navigation", "robot", "near_elevator", "corridor", "1"]
+    handle_client_connection(a)
+    # try:
+    #     HOST = socket.gethostbyname("localhost")
+    #     PORT = 1770
+    #     print('HOST:', HOST)
+    #     start_tcp_server(HOST, PORT)
+    # except rospy.ROSInterruptException:
+    #     pass
 
-    # robot_navigation_action("robot", "location", "outside_lab211", floor="1")
+    #pick_client("$robot1", "$object1","$location1")
+    #robot_navigation_action("robot", "location", "outside_lab211", floor="1")
     # pick_unknown_action("robot", "can", "outside_lab211")
